c178353659620936f2756026a8f74eb98da2e9f4
Wasay Rizwani <wasay.rizwani@tuni.fi>
2024-10-28 19:16:57 +0200

    main

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,6 +18,7 @@ function Home() {
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
+                
               </h1>
 
               <h1 className="heading-name">
