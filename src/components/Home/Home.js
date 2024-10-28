85a20c7c50b94a9753e84e141b9c73537dd893eb
Wasay Rizwani <wasay.rizwani@tuni.fi>
2024-10-28 19:15:56 +0200

    Update Home.js
    hellp

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Muhammad Hasham</strong>
+                <strong className="main-name"> DailyRoutineApps</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
